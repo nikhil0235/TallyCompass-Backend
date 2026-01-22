9c4a6066959ab7a14ee8fbd047cb8f0e522fb5ea
nikh0235 <rajnikhil705@gmail.com>
2026-01-22 12:33:05 +0530

    Added Frontend URI to fix cors issue

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,8 @@
 const corsOptions = {
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://tallycompass.netlify.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
