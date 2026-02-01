c61361bfd9a530608bfe7632166584c8c567dc81
Mohamadsohil Momin <mohamadsohil.m@tallysolutions.com>
2026-02-01 16:11:21 +0530

    [] Get all Form Responses

--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { submitResponse } = require('../controllers/formController');
+const { submitResponse, getAllFormResponses, getFormResponseById } = require('../controllers/formController');
 
 router.post('/', submitResponse);
+router.get('/', getAllFormResponses);
+router.get('/:id', getFormResponseById);
 
 module.exports = router;
